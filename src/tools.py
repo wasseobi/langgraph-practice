bc0bef2b6543bea3f0f2c638a2f64f354e2846c0
jooeun921 <jooeun921@gmail.com>
2025-05-12 23:22:01 +0900

    🪡 tool : google_search

--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,6 @@
-
 import datetime
 
+from langchain_google_community import GoogleSearchAPIWrapper
 
 def what_time_is_it() -> str:
     """Get the current time.
@@ -11,5 +11,24 @@ def what_time_is_it() -> str:
 
     return datetime.datetime.now().strftime("%H:%M:%S")
 
+def google_search(topic: str, num_results: int) -> list:
+    """
+    Perform a Google search and retrieve the results.
 
-tools = [what_time_is_it]
+    Args:
+        topic (str): The search query.
+        num_results (int): The number of search results to retrieve.
+
+    Returns:
+        list: A list of search results.
+    """
+    search = GoogleSearchAPIWrapper()
+    results = []
+
+    res = search.results(topic, num_results=num_results)
+    results.extend(res)
+
+    return results
+
+
+tools = [what_time_is_it, google_search]
